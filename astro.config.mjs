b4b71c491b81b0a401350ba88c46b5ceb1067fec
r2ditto <ryan.ebro@yahoo.com>
2024-12-01 13:52:49 -0500

    update configs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,37 @@
 import tailwind from "@astrojs/tailwind";
+import storyblok from "@storyblok/astro";
 import { defineConfig } from "astro/config";
+import { loadEnv } from "vite";
+import basicSsl from "@vitejs/plugin-basic-ssl";
+const env = loadEnv("", process.cwd(), "STORYBLOK");
+import react from "@astrojs/react";
 
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import pagefind from "astro-pagefind";
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://astro-micro.vercel.app",
-  integrations: [tailwind(), sitemap(), mdx(), pagefind()],
+  integrations: [
+    tailwind(),
+    sitemap(),
+    mdx(),
+    // storyblok({ accessToken: env.STORYBLOK_TOKEN }),
+    pagefind(),
+    react(),
+  ],
   markdown: {
     shikiConfig: {
       theme: "css-variables",
     },
+  },
+  vite: {
+    // plugins: [basicSsl()],
+    // server: {
+    //   https: true,
+    // },
   },
 });
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -8,6 +8,20 @@ export default {
       fontFamily: {
         sans: ["Geist Sans", ...defaultTheme.fontFamily.sans],
         mono: ["Geist Mono", ...defaultTheme.fontFamily.mono],
+        bungee: ["Bungee Shade", ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        customNeutral: {
+          100: "#FCFCFA",
+          200: "#FAF9F7",
+          300: "#F5F4F0",
+          400: "#EDEBE4",
+          500: "#E5E1D7",
+          600: "#CFC5B0",
+          700: "#AB9878",
+          800: "#AB9878",
+          900: "#66492B",
+        },
       },
     },
   },
