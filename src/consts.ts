64e85886773bc8ca0fd9dd26e7f81937ebb7cb9a
r2ditto <ryan.ebro@yahoo.com>
2025-01-25 22:11:12 -0500

    update constants

--- src/consts.ts
+++ src/consts.ts
@@ -1,9 +1,9 @@
 import type { Metadata, Site, Socials } from "@types";
 
 export const SITE: Site = {
-  TITLE: "Astro Micro",
-  DESCRIPTION: "Astro Micro is an accessible and lightweight blog.",
-  EMAIL: "trevortylerlee@gmail.com",
+  TITLE: "R2",
+  DESCRIPTION: "Notes on frontend development",
+  EMAIL: "ryan.ebro@yahoo.com",
   NUM_POSTS_ON_HOMEPAGE: 5,
   NUM_PROJECTS_ON_HOMEPAGE: 3,
 };
@@ -26,15 +26,19 @@ export const PROJECTS: Metadata = {
 
 export const SOCIALS: Socials = [
   {
-    NAME: "X (formerly Twitter)",
-    HREF: "https://twitter.com/boogerbuttcheeks",
+    NAME: "Bluesky",
+    HREF: "https://bsky.app/profile/r2-ok.bsky.social",
   },
   {
     NAME: "GitHub",
-    HREF: "https://github.com/trevortylerlee",
+    HREF: "https://github.com/r2ditto",
   },
   {
     NAME: "LinkedIn",
-    HREF: "https://www.linkedin.com/in/trevortylerlee",
+    HREF: "https://www.linkedin.com/in/ryanebro",
+  },
+  {
+    NAME: "Instagram",
+    HREF: "https://www.instagram.com/ryvnrey",
   },
 ];
