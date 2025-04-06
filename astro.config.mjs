9f9fba4db8d9738331a4204a13d5e05b744bfd37
r2ditto <ryan.ebro@yahoo.com>
2025-04-05 20:59:13 -0400

    update astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,27 @@
-import tailwind from "@astrojs/tailwind";
-import { defineConfig } from "astro/config";
+import sanity from "@sanity/astro";
 
+import react from "@astrojs/react";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import pagefind from "astro-pagefind";
+import tailwind from "@astrojs/tailwind";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://astro-micro.vercel.app",
-  integrations: [tailwind(), sitemap(), mdx(), pagefind()],
+  integrations: [
+    tailwind(),
+    sitemap(),
+    mdx(),
+    pagefind(),
+    sanity({
+      projectId: "dlra7hh7",
+      dataset: "production",
+      useCdn: false,
+    }),
+    react(),
+  ],
   markdown: {
     shikiConfig: {
       theme: "css-variables",
