265fb9e807db710f609df492b312e28488f1fca7
r2ditto <ryan.ebro@yahoo.com>
2025-04-05 22:02:31 -0400

    add post schema

---
+++ schemas/post.js
@@ -0,0 +1,48 @@
+export default {
+  name: "post",
+  title: "Blog Posts",
+  type: "document",
+  fields: [
+    {
+      name: "title",
+      title: "Title",
+      type: "string",
+      validation: (Rule) => Rule.required(),
+    },
+    {
+      name: "slug",
+      title: "Slug",
+      type: "slug",
+      options: {
+        source: "title",
+        maxLength: 96,
+      },
+      validation: (Rule) => Rule.required(),
+    },
+    {
+      name: "description",
+      title: "Description",
+      type: "text",
+      validation: (Rule) => Rule.required(),
+    },
+    {
+      name: "publishedAt",
+      title: "Published at",
+      type: "datetime",
+      validation: (Rule) => Rule.required(),
+    },
+    {
+      name: "mainImage",
+      title: "Main image",
+      type: "image",
+      options: {
+        hotspot: true,
+      },
+    },
+    {
+      name: "body",
+      title: "Body",
+      type: "blockContent",
+    },
+  ],
+};
